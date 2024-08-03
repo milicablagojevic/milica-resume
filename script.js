240ce1a59a3082d24c935ebe0c584b1f08ea56c1
milicablagojevic <milica.comi24@gmail.com>
2024-08-03 16:28:19 +0200

    darkmode

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+// Get the toggle button and body elements
+
+const toggleButton = document.querySelector('.toggle-button');
+const body = document.body;
+
+// Add event listener to toggle button
+
+toggleButton.addEventListener('click', () => {
+  // Toggle the dark theme class on the body element
+  body.classList.toggle('dark-theme');
+
+  // Update the toggle button text
+  if (body.classList.contains('dark-theme')) {
+    toggleButton.textContent = 'Light Mode';
+  } else {
+    toggleButton.textContent = 'Dark Mode';
+  }
+
+});
